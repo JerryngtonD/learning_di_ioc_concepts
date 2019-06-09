a28d0050d0f9fdf0b2172ed58c70ba7c0512e712
jerryngton <jerryngtond@gmail.com>
2019-06-09 19:33:17 +0300

    examples of using DI via constr / method / property

---
+++ src/main/java/guru/springframework/didemo/controllers/MyController.java
@@ -0,0 +1,10 @@
+package guru.springframework.didemo.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public void sayHello() {
+        System.out.println("hello");
+    }
+}
